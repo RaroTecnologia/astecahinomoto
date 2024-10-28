33f60a3974f49e5020a10c97a76951be5d4b5913
Tiago Levorato <tiagolevorato@treslados.group>
2024-10-27 22:34:31 -0300

    vite js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/slider.js'],
+            input: [
+                'resources/css/app.css',
+                 'resources/js/app.js', 
+                 'resources/js/slider.js',
+                 'resources/js/produto.js',
+                 'resources/js/copiaUrl.js',
+                 'resources/js/editor.js',
+                 'resources/js/submenu.js',
+                 'resources/js/toast.js',
+                ],
             refresh: true,
         }),
     ],
