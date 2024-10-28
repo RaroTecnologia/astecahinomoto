7c854b53dd380ab3ae4321cf80b835207b705c30
Tiago Levorato <tiagolevorato@treslados.group>
2024-10-27 22:30:22 -0300

    ajuste vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/slider.js'],
             refresh: true,
         }),
     ],
-    server: {
-        host: 'localhost', // Força o uso de IPv4 em vez de IPv6
-    },
 });
