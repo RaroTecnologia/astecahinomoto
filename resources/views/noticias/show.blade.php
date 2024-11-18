99faa1eb1b79294149abc116798839b3bf85dd8a
Tiago Levorato <tiagolevorato@treslados.group>
2024-11-18 12:32:48 -0300

    ajuste path img noticia

--- resources/views/noticias/show.blade.php
+++ resources/views/noticias/show.blade.php
@@ -46,7 +46,7 @@
             <x-card-item
                 title="{{ $noticia->titulo }}"
                 description="{{ Str::limit($noticia->resumo, 100) }}"
-                image="{{ $noticia->imagem_url }}"
+                image="{{ asset('storage/noticias/thumbnails/' . $noticia->imagem) }}"
                 link="{{ route('noticias.show', ['categoria' => $noticia->categoria->slug, 'slug' => $noticia->slug]) }}"
                 linkText="Ler Mais" />
             @endforeach
